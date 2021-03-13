ce97a1a5948e06aade7fe871692511b803238fbc
Muntasir Mamun Nahid <muntasirnahid87@gmail.com>
2021-03-13 20:22:37 +0600

    snakeLAdder

---
+++ Snake_And_Ladder_Game.cpp
@@ -0,0 +1,172 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+
+void board();
+void play_dice(int &score);
+void gamescore(char name1[], char name2[], int p1, int p2);
+void draw_line(int n, char ch);
+
+int main()
+{
+	int player1 = 0, player2 = 0, lastpos;
+	char p1name[50], p2name[50];
+
+	system("cls");
+
+	srand((unsigned)time(NULL));
+
+	draw_line(50, '='); cout << endl;
+	draw_line(50, '='); cout << endl;
+	draw_line(50, '=');
+	cout << "\n\t\t SNAKE LADDER GAME " << endl;
+	draw_line(50, '='); cout << endl;
+	draw_line(50, '='); cout << endl;
+	draw_line(50, '=');
+	cout << "\n\nFisrt Player Name :";
+	gets(p1name);
+	cout << "\n\nSecond Player Name :";
+	gets(p2name);
+	while (player1 < 100 && player2 < 100)
+	{
+		board();
+		gamescore(p1name, p2name, player1, player2);
+		cout << "\n \n >>> " << p1name << " ,Now Your Turn! Press any key to Roll the dice.";
+		getch();
+		lastpos = player1;
+		play_dice(player1);
+		if (player1 < lastpos)
+			cout << "\n\a Oopps! Snake Found!! Now You are at position " << player1 << endl;
+		else if (player1 > lastpos + 6)
+			cout << "\n \n Great!! You Got a Ladder! Now You are at position " << player1 << endl;
+
+		cout << "\n \n >>> " << p2name << " ,Now Your Turn! Press any key to Roll the dice.";
+		getch();
+		lastpos = player2;
+		play_dice(player2);
+		if (player2 < lastpos)
+			cout << "\n \a opps! Snake Found!! Now You are at position " << player2 << endl;
+		else  if (player2 > lastpos + 6)
+			cout << "\n \n Great!! You Got a Ladder! Now You are at position " << player2 << endl;
+		getch();
+	}
+	system("cls");
+	cout << "\n\n\n";
+	draw_line(52, '#');
+	cout << "\n \t \tRESULT \n \n";
+	draw_line(52, '#');
+	cout << endl;
+	gamescore(p1name, p2name, player1, player2);
+	cout << "\n \n \n";
+	if (player1 <= player2)
+		cout << "Congratulations!! " << p2name << " have won the GAME!" << endl;
+	else
+		cout << "Congratulations!! " << p1name << " have won the GAME!" << endl;
+	draw_line(52, '#');
+	getch();
+	return 0;
+
+}
+void draw_line(int n, char ch)
+{
+	for (int i = 0; i < n; i++)
+		cout << ch;
+}
+void board()
+{
+	system("cls");
+	cout << "\n \n";
+	draw_line(52, '-');
+	cout << "\n|Snake at position \t|\t" << "Ladder at position |\n";
+	draw_line(52, '-');
+	cout << endl;
+	cout << "| From 98 to 28 \t|\t" << "From 8  to 26      |\n";
+	cout << "| From 92 to 51 \t|\t" << "From 21 to 82      |\n";
+	cout << "| From 83 to 19 \t|\t" << "From 43 to 77      |\n";
+	cout << "| From 69 to 33 \t|\t" << "From 50 to 91      |\n";
+	cout << "| From 59 to 17 \t|\t" << "From 62 to 96      |\n";
+	cout << "| From 48 to 9  \t|\t" << "From 66 to 87      |\n";
+	cout << "| From 46 to 5  \t|\t" << "From 80 to 100     |\n";
+	draw_line(52, '-');
+	cout << endl;
+}
+void gamescore(char name1[], char name2[], int p1, int p2)
+{
+	cout << "\n";
+	draw_line(52, '-');
+	cout << "\n\t\t GAME STATUS\t\t" << endl;
+	draw_line(52, '-');
+	cout << "\n< " << name1 << " is at position " << p1 << endl;
+	cout << "< " << name2 << " is at position " << p2 << endl;
+}
+
+void play_dice(int & score)
+{
+	int dice;
+	dice = 1 + rand() % 6; //1-6 range
+	cout << "\nYou got " << dice << " Point !! ";
+	score += dice;
+	cout << "Now you are at position " << score;
+	switch (score)
+	{
+	case 98:
+		score = 28;
+		break;
+
+	case 92:
+		score = 51;
+		break;
+
+	case 83:
+		score = 19;
+		break;
+
+	case 69:
+		score = 33;
+		break;
+
+	case 59:
+		score = 17;
+		break;
+
+	case 48:
+		score = 9;
+		break;
+
+	case 46:
+		score = 5;
+		break;
+
+	case 8:
+		score = 26;
+		break;
+
+	case 21:
+		score = 82;
+		break;
+
+	case 43:
+		score = 77;
+		break;
+
+	case 50:
+		score = 91;
+		break;
+
+	case 62:
+		score = 96;
+		break;
+
+	case 66:
+		score = 87;
+		break;
+
+	case 80:
+		score = 100;
+		break;
+	}
+
+}
